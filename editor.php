0488b5649f6cf6920cb0ad14acf1abee28072e24
kubasi3 <jakub@semeniuk.cz>
2019-09-27 10:31:46 +0200

    First commit

---
+++ editor.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en" xml:lang="en" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <link rel="stylesheet" type="text/css" href="./css/style.css">
+    <title>Maturita</title>
+</head>
+<body>
+</body>
+<script src="./js/panel.js"></script>
+</html>
---
+++ js/panel.js
@@ -0,0 +1,14 @@
+var Panel = function () {
+    var body = document.getElementsByTagName('body')[0];
+
+    var Menu1 = function () {
+        var workPlace = document.createElement('div');
+        
+
+        body.appendChild(workPlace);
+    }
+
+    var menu1 = new Menu1();
+}
+
+var panel = new Panel();
